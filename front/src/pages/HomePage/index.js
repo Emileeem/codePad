36f32f593f7c51472fce7fb357b591b364ffa20e
Emyli Quadros <babaloogames@gmail.com>
2024-02-14 11:54:05 -0300

    a

--- front/src/pages/HomePage/index.js
+++ front/src/pages/HomePage/index.js
@@ -1,10 +1,10 @@
 import { Home } from "../../components/Home";
 import { Menu } from "../../components/Menu";
 export function HomePage() {
-    return(
-        <>
-            <Menu/>
-            <Home/>
-        </>
-    )
+  return (
+    <>
+      <Menu />
+      <Home />
+    </>
+  );
 }
